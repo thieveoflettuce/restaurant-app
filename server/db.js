8f1432808e67224a7fd9eaf78e40175c5682f7a1
martonchelik <84589903+martonchelik@users.noreply.github.com>
2026-04-06 22:57:47 +0300

    deploy fixes 2

--- server/db.js
+++ server/db.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
 });
 
 pool.connect((err) => {
--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const pool = require('./db');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
